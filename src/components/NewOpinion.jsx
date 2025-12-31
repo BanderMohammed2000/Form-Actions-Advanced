6ac25193f7f5d7aa3e4f51d6f441c3b5f53db2b3
Bander Btati <bander.mohammed.2000@gmail.com>
2025-12-31 04:21:16 +0300

    the project is done

--- src/components/NewOpinion.jsx
+++ src/components/NewOpinion.jsx
@@ -1,7 +1,12 @@
-import { useActionState } from "react";
+import { useActionState, use } from "react";
+
+import { OpinionsContext } from "../store/opinions-context.jsx";
+import Submit from "./Submit.jsx";
 
 export function NewOpinion() {
-  function shareOpinionAction(prevState, formData) {
+  const { addOpinion } = use(OpinionsContext);
+
+  async function shareOpinionAction(prevState, formData) {
     const title = formData.get("title");
     const body = formData.get("body");
     const userName = formData.get("userName");
@@ -32,7 +37,7 @@ export function NewOpinion() {
     }
 
     // submit to backend
-
+    await addOpinion({ title, body, userName });
     return { errors: null };
   }
 
@@ -82,9 +87,7 @@ export function NewOpinion() {
           </ul>
         )}
 
-        <p className="actions">
-          <button type="submit">Submit</button>
-        </p>
+        <Submit />
       </form>
     </div>
   );
